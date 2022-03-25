c1605b0e0aed7561843f939efab559f44c107401
rehamYahia <asmaa115yahia@gmail.com>
2022-03-25 19:28:10 +0200

    Add Fragment

--- app/src/main/java/com/example/musicplayer/HomeActivity.kt
+++ app/src/main/java/com/example/musicplayer/HomeActivity.kt
@@ -3,13 +3,32 @@ package com.example.musicplayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        add_fragment(MusicFragment())
+        music_image.setOnClickListener {
+            add_fragment(MusicFragment())
+        }
+        fav_image.setOnClickListener {
+            add_fragment(FavouritFragment())
+        }
+        playlist_image.setOnClickListener {
+            add_fragment(PlaylistFragment())
+        }
 
 
+    }
+
+    private fun add_fragment(fragment: Fragment)
+    {
+        var transation = supportFragmentManager.beginTransaction()
+        transation.replace(R.id.pager,fragment)
+        transation.commit()
     }
 
 }
