65d2bfc1624d5a7d7e653bf3eaa95f7ec4fd53eb
rehamYahia <asmaa115yahia@gmail.com>
2022-03-25 16:41:19 +0200

    first commit

---
+++ app/src/main/java/com/example/musicplayer/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.musicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+    }
+
+}
